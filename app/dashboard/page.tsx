088b73aaa3b6d91c88bc2b7f1bdbbdb6da66d367
Joaquin Florentino <joaquinflorentino3@gmail.com>
2026-05-30 04:04:48 -0400

    fetch user top tracks and artists from spotify api

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -27,23 +27,37 @@ export default async function Dashboard() {
 
   const accessToken = session.provider_token
 
-  const response = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=5', {
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
+  const [tracksResponse, artistsResponse] = await Promise.all([
+    fetch('https://api.spotify.com/v1/me/top/tracks?limit=5', {
+        headers: { Authorization: `Bearer ${accessToken} ` }
+    }),
+    fetch('https://api.spotify.com/v1/me/top/artists?limit=5', {
+        headers: { Authorization: `Bearer ${accessToken}` }
+    })
+  ])
 
-  const topTracks = await response.json()
+  const topTracks = await tracksResponse.json();
+  const topArtists = await artistsResponse.json();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-2xl font-bold">Your Top Tracks</h1>
+  <main className="flex min-h-screen flex-col items-center justify-center gap-8">
+    <section>
+      <h2 className="text-xl font-bold mb-4">Top Tracks</h2>
       {topTracks.items?.map((track: any) => (
-        <div key={track.id} className="text-center">
+        <div key={track.id} className="mb-2">
           <p className="font-semibold">{track.name}</p>
           <p className="text-gray-500">{track.artists[0].name}</p>
         </div>
       ))}
-    </main>
-  )
+    </section>
+    <section>
+      <h2 className='text-xl font-bold mb-4'>Top Artists</h2>
+      {topArtists.items?.map((artist: any) => (
+        <div key={artist.id} className='mb-2'>
+            <p className='font-semibold'>{artist.name}</p>
+        </div>
+      ))}
+    </section>
+  </main>
+)
 }
